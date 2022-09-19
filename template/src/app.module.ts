0239c189fa30f43f046d0526ed79cf52e0b6d4ae
VoVanThanh <thanhvo618@gmail.com>
2022-09-19 11:02:19 +0700

    template logger middleware

--- template/src/app.module.ts
+++ template/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
+import { LoggerMiddleware } from './middlewares';
 
 @Module({
   imports: [
@@ -16,4 +17,8 @@ import { TodoModule } from './todo/todo.module';
     TodoModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
