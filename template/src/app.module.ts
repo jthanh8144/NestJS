3185109309e9b655dbd7450dbae00dae01c7378c
VoVanThanh <thanhvo618@gmail.com>
2022-09-21 11:16:05 +0700

    template pipe

--- template/src/app.module.ts
+++ template/src/app.module.ts
@@ -1,10 +1,13 @@
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
-import { LoggerMiddleware } from './middlewares';
+import { LoggerMiddleware } from '@common/middlewares';
+import { AllExceptionsFilter } from '@common/exceptions';
+import { ValidationPipe } from '@common/pipes';
 
 @Module({
   imports: [
@@ -15,6 +18,16 @@ import { LoggerMiddleware } from './middlewares';
     AuthModule,
     UserModule,
     TodoModule,
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class AppModule {
--- template/src/main.ts
+++ template/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dataSource from '@shared/configs/data-source.config';
+import { getLogLevels } from '@common/utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: getLogLevels(process.env.NODE_ENV === 'production'),
+  });
   await app.listen(+process.env.PORT || 3000);
   await dataSource.initialize();
 }
